01b8e7d4d6f14852e31d56dc10af770908dc224e
Todd <Todd@Todd-PC>
2013-03-12 18:28:29 -0500

    Started adding LGPL coments

---
+++ test/org/talwood/marcworx/rules/MarcRulesContainerTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.talwood.marcworx.rules;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Todd
+ */
+public class MarcRulesContainerTest {
+    
+    public MarcRulesContainerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+}
